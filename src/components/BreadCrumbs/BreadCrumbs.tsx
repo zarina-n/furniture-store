900a4e2c0a7be178cbe821e38c702d02fd3e1561
Zarina <zarina_nugmanova@mail.ru>
2025-01-07 13:11:12 -0500

    added more logic to Breadcrumbs component

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -3,27 +3,56 @@
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import styles from './Breadcrumbs.module.css'
+import cn from 'classnames'
+import { useState } from 'react'
+
+const links = [
+  'All',
+  'Rest',
+  'Work',
+  'Kitchen',
+  'Children',
+  'Bath',
+  'Favorites',
+]
 
 export default function Breadcrumbs() {
+  // TODO: refactor this component
   const paths = usePathname() ?? ''
-  const pathNames = paths.split('/').filter((path) => path)
+  const categoryPathName = paths.split('/').filter((path) => path)[1]
+
+  const [areLinksShown, setAreLinksShown] = useState(false)
 
   return (
     <ul className={styles.breadcrumb}>
       <li className={styles.list_link}>
         <Link href={'/'}>Home</Link>
       </li>
-      {pathNames.map((link, index) => {
-        const href = `/${pathNames.slice(0, index + 1).join('/')}`
-
-        const itemLink = link[0].toUpperCase() + link.slice(1, link.length)
-
-        return (
-          <li className={styles.list_link} key={index}>
-            <Link href={href}>{itemLink}</Link>
-          </li>
-        )
-      })}
+      <li className={styles.list_link}>
+        <Link href={'/catalog'}>Catalog</Link>
+      </li>
+      <li className={cn(styles.list_link, styles.list_link_category)}>
+        <Link
+          href={`/catalog/${categoryPathName}`}
+          onMouseEnter={() => setAreLinksShown(true)}
+          onMouseLeave={() => setAreLinksShown(false)}
+        >
+          {categoryPathName[0].toUpperCase() +
+            categoryPathName.slice(1, categoryPathName.length)}
+        </Link>
+        <ul
+          className={styles.categories_list}
+          onMouseEnter={() => setAreLinksShown(true)}
+          onMouseLeave={() => setAreLinksShown(false)}
+        >
+          {areLinksShown &&
+            links.map((link) => (
+              <li key={link}>
+                <Link href={`/catalog/${link}`}>{link}</Link>
+              </li>
+            ))}
+        </ul>
+      </li>
     </ul>
   )
 }
